0cb1ede5c0d9b5e057cbe8ca3c6e6cd226028301
ninja119347 <1193471306@qq.com>
2024-07-29 20:19:19 +0800

    mulDel

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@
     </div>
     <!-- query -->
     <div class="query-box">
-      <el-input class="query-input" v-model="queryInput"  placeholder="Please input" />
-      <el-button type="primary" @click="handleAdd" >Add</el-button>
+      <el-input class="query-input" v-model="queryInput"  placeholder="Please search by name" />
+      <div class="btn-list">
+        <el-button type="primary" @click="handleAdd" >Add</el-button>
+      <el-button type="danger" @click="handleDelList"   v-if="multipleSelection.length>0"  >Mul-delete</el-button>
+      </div>
+      
     </div>
     <!-- table -->  
     <el-table
@@ -17,23 +21,26 @@
     style="width: 100%"
     @selection-change="handleSelectionChange">
     <el-table-column type="selection" width="55" />
-    <el-table-column fixed prop="date" label="Date" width="150" />
     <el-table-column prop="name" label="Name" width="120" />
+    <el-table-column prop="email" label="Email" width="200" />
     <el-table-column prop="state" label="State" width="120" />
-    <el-table-column prop="city" label="City" width="120" />
-    <el-table-column prop="address" label="Address" width="600" />
-    <el-table-column prop="zip" label="Zip" width="120" />
+    <el-table-column prop="phone" label="Phone" width="120" />
+    <el-table-column prop="address" label="Address" width="300" />
     <el-table-column fixed="right" label="Operations" min-width="120">
-      <template #default>
-        <el-button link type="primary" size="small" @click="handleRowClick">
-          Detail
+      <template #default="scope">
+        <el-button link
+          type="primary"
+          size="small"
+          @click="handleRowDel(scope.row)" 
+          style="color: #F56C6C;">
+          Delete
         </el-button>
         <el-button link type="primary" size="small">Edit</el-button>
       </template>
     </el-table-column>
     </el-table>
     <!------------------- dialog --------------------->
-    <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+    <el-dialog v-model="dialogFormVisible" :title = "dialogType==='add'? 'Add':'Edit'" width="500">
     <el-form :model="tableForm">
       <el-form-item label="姓名" :label-width="80">
         <el-input v-model="tableForm.name" autocomplete="off" />
@@ -57,16 +64,12 @@
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">
+        <el-button type="primary" @click="dialogconfirm">
           Confirm
         </el-button>
       </div>
     </template>
   </el-dialog>
-
-
-
-
   </div>
 </template>
 
@@ -75,40 +78,49 @@ import { ref } from 'vue'
 // data
 let queryInput = ref('')
 let tableData = ref([ {
-    date: '2016-05-03',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+    id:"1",
+    name:"Tom1",
+    email:"1193471036@qq.com",
+    phone:"1193471036",
+    state:"California",
+    address:"广东省广州市天河区",
+
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+    id:"2",
+    name:"Tom2",
+    email:"1193471036@qq.com",
+    phone:"1193471036",
+    state:"California",
+    address:"广东省广州市天河区",
+
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Home',
+    id:"3",
+    name:"Tom3",
+    email:"1193471036@qq.com",
+    phone:"1193471036",
+    state:"California",
+    address:"广东省广州市天河区",
+
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    state: 'California',
-    city: 'Los Angeles',
-    address: 'No. 189, Grove St, Los Angeles',
-    zip: 'CA 90036',
-    tag: 'Office',
+    id:"4",
+    name:"Tom4",
+    email:"1193471036@qq.com",
+    phone:"1193471036",
+    state:"California",
+    address:"广东省广州市天河区",
+
+  },
+  {
+    id:"5",
+    name:"Tom5",
+    email:"1193471036@qq.com",
+    phone:"1193471036",
+    state:"California",
+    address:"广东省广州市天河区",
+
   },])
 let multipleSelection = ref([])
 let dialogFormVisible = ref(false)
@@ -119,17 +131,52 @@ let tableForm = ref({
   state: '在职',
   address: '广东省',
 },)
+let dialogType = ref('add')
 
   // methods
+  //选中
 const handleSelectionChange = (val) => {
-  multipleSelection.value = val
-  console.log(multipleSelection.value)
+  multipleSelection.value = []
+  val.forEach(element => {
+      multipleSelection.value.push(element.id)
+    })
 }
+// 添加
 let handleAdd = () => {
   dialogFormVisible.value = true
+  dialogType.value = 'add'
+  tableForm.value={}
 }
+//
 let handleRowClick = () => {
   console.log('click')
+}
+// 确认
+let dialogconfirm = () => {
+  dialogFormVisible.value = false
+  //1. 拿到数据
+
+  //2. 添加到table中
+  tableData.value.push({id:(tableData.value.length+1).toString(),...tableForm.value}
+    )
+}
+//删除一条
+let handleRowDel = ({id}) => {
+  console.log(id)
+  //1. 通过id获取条目对应的索引值
+  let index = tableData.value.findIndex(item => item.id === id)
+  //2. 通过索引值删除对应条目
+  tableData.value.splice(index, 1)
+  // tableData.value = tableData.value.filter(item => item.id !== row.id)
+}
+//删除选中
+let handleDelList = () => {
+  //1. 通过id获取条目对应的索引值
+  multipleSelection.value.forEach(id => {
+    handleRowDel({id})
+  })
+  //2. 通过索引值删除对应条目
+  // tableData.value = tableData.value.filter(item => !indexs.includes(item.id))
 }
 </script>
 
