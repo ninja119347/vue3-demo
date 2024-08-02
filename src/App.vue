b65def8390c3426005dc43cd67fdfb146eb355f6
ninja119347 <1193471306@qq.com>
2024-08-02 17:18:30 +0800

    add axois crud function

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </div>
     <!-- query -->
     <div class="query-box">
-      <el-input class="query-input" v-model="queryInput"  placeholder="Please search by name" @input="handleQueryName" />
+      <el-input class="query-input" v-model="queryInput"  placeholder="Please search by name" @change="handleQueryName" />
       <div class="btn-list">
         <el-button type="primary" @click="handleAdd" >Add</el-button>
       <el-button type="danger" @click="handleDelList"   v-if="multipleSelection.length>0"  >Mul-delete</el-button>
@@ -21,11 +21,16 @@
     style="width: 100%"
     @selection-change="handleSelectionChange">
     <el-table-column type="selection" width="55" />
+    <el-table-column prop="id" label="id" width="120"/>
+    <el-table-column prop="postId" label="post_id" width="120"/>
+    <el-table-column prop="deptId" label="dept_id" width="120"/>
     <el-table-column prop="username" label="Name" width="120" />
+    <el-table-column prop="nickname" label="nickname" width="120"/>
     <el-table-column prop="email" label="Email" width="200" />
     <el-table-column prop="status" label="State" width="120" />
     <el-table-column prop="phone" label="Phone" width="120" />
     <el-table-column prop="address" label="Address" width="300" />
+    <el-table-column prop="createTime" label="createTime" width="200" />
     <el-table-column fixed="right" label="Operations" min-width="120">
       <template #default="scope">
         <el-button link
@@ -39,11 +44,24 @@
       </template>
     </el-table-column>
     </el-table>
+    <el-pagination
+     background
+     layout="prev, pager, next" 
+     :total="total" 
+     v-model:current-page="curPage"
+     :page-size="pageSize"
+     @current-change="handleChangePage"
+     @size-change="handleUpdatePageSize" 
+     style="display:flex; justify-content:center; margin-top:10px" 
+     />
     <!------------------- dialog --------------------->
     <el-dialog v-model="dialogFormVisible" :title = "dialogType==='add'? 'Add':'Edit'" width="500">
     <el-form :model="tableForm">
       <el-form-item label="姓名" :label-width="80">
-        <el-input v-model="tableForm.name" autocomplete="off" />
+        <el-input v-model="tableForm.username" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="密码" :label-width="80">
+        <el-input v-model="tableForm.password" autocomplete="off" />
       </el-form-item>
       <el-form-item label="邮箱" :label-width="80">
         <el-input v-model="tableForm.email" autocomplete="off" />
@@ -74,81 +92,69 @@
 import { ta } from 'element-plus/es/locales.mjs';
 import { ref } from 'vue'
 import request from './utils/request.js'
+import { tabsEmits } from 'element-plus';
 
 // data
 let queryInput = ref('')
-let tableData = ref([ {
-    id:"1",
-    name:"Tom1",
-    email:"1193471036@qq.com",
-    phone:"1193471036",
-    state:"California",
-    address:"广东省广州市天河区",
-
-  },
-  {
-    id:"2",
-    name:"Tom2",
-    email:"1193471036@qq.com",
-    phone:"1193471036",
-    state:"California",
-    address:"广东省广州市天河区",
-
-  },
-  {
-    id:"3",
-    name:"Tom3",
-    email:"1193471036@qq.com",
-    phone:"1193471036",
-    state:"California",
-    address:"广东省广州市天河区",
-
-  },
-  {
-    id:"4",
-    name:"Tom4",
-    email:"1193471036@qq.com",
-    phone:"1193471036",
-    state:"California",
-    address:"广东省广州市天河区",
-
-  },
-  {
-    id:"5",
-    name:"Tom5",
-    email:"1193471036@qq.com",
-    phone:"1193471036",
-    state:"California",
-    address:"广东省广州市天河区",
-
-  },])
+let tableData = ref()
 let multipleSelection = ref([])
 let dialogFormVisible = ref(false)
 let tableForm = ref({
-  username: '张三',
-  email: '123@qq.com ',
-  phone: '1192371384',
-  status: '在职',
-  address: '广东省',
+
 },)
 let dialogType = ref('add')
-let tableDataCopy = Object.assign([],tableData.value)
+let tableDataCopy = []
+let total = ref(50)
+let curPage = ref(1)
+let pageSize = ref(10)
   // methods
+  const handleUpdatePageSize = (newSize) => {
+  pageSize.value = newSize
+  console.log(newSize)
+  getTableData()
+}
+const handleChangePage =(val)=>{
+  getTableData(val)
+}
+const handleChangeSize =(val)=>{
+  pageSize.value = val
+  console.log(val)
+  getTableData()
+}
+  // 格式化日期函数
+function formatDate(date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
 const getTableData = async (cur = 1) => {
-  let res = await request.get('api/user/list',  {pageSize: 10, pageNum:cur})
+  // let res = await request.get('api/user/list',  {pageSize: 5, pageNum:cur})
+  let res = await request.get(`api/user/list?page=${cur}&pageSize=9`)
   tableData.value = res.data
+  // tableDataCopy= Object.assign([],tableData.value)
   console.log(res)
 }
 getTableData()
-let handleQueryName = (val) => {
-  tableData.value = tableDataCopy
-  // console.log(queryInput.value)
-  if (val.length>0){
-    tableData.value = tableData.value.filter(item => (item.name).toLowerCase().match(val.toLowerCase()))
-  }else{
-    console.log('length=0')
-    tableData.value = tableDataCopy
+//查询数据by name
+let handleQueryName = async (val) => {
+  // tableData.value = tableDataCopy
+  // // console.log(queryInput.value)
+  // if (val.length>0){
+  //   tableData.value = tableData.value.filter(item => (item.username).toLowerCase().match(val.toLowerCase()))
+  // }else{
+  //   console.log('length=0')
+  //   tableData.value = tableDataCopy
+  // }
+  if (val.length === 0) {
+    await getTableData()
+    return
   }
+  let res = await request.get(`api/user/${val.toString()}`)
+  tableData.value = res.data
   
 }
   //选中
@@ -169,28 +175,30 @@ let handleRowClick = () => {
   console.log('click')
 }
 // 确认
-let dialogconfirm = () => {
+let dialogconfirm =async () => {
   dialogFormVisible.value = false
   if(dialogType.value === 'add') {
   //1. 拿到数据
   //2. 添加到table中
-    tableData.value.push({id:(tableData.value.length+1).toString(),...tableForm.value})
+    console.log("add data: ",tableForm.value)
+    let res = await request.post('register',{
+      ...tableForm.value,
+      // id: (tableData.value.length + 1),
+    })
+    //刷新数据
+    await getTableData(curPage.value)
+    // tableData.value.push({id:(tableData.value.length+1).toString(),createTime:(formatDate(new Date())),...tableForm.value})
   } else {
-    let index = tableData.value.findIndex(item => item.id === tableForm.value.id)
-    tableData.value[index] = tableForm.value
+    await request.put('api/updateUser',{
+      ...tableForm.value,
+    })
+    await getTableData(curPage.value)
   }
-  tableDataCopy = Object.assign([],tableData.value)
-  
 }
 //删除一条
-let handleRowDel = ({id}) => {
-  console.log(id)
-  //1. 通过id获取条目对应的索引值
-  let index = tableData.value.findIndex(item => item.id === id)
-  //2. 通过索引值删除对应条目
-  tableData.value.splice(index, 1)
-  // tableData.value = tableData.value.filter(item => item.id !== row.id)
-  tableDataCopy = Object.assign([],tableData.value)
+const handleRowDel = async ({id}) => {
+  await request.delete(`api/delete/`+id)
+  await getTableData(curPage.value)
 }
 //删除多个选中
 let handleDelList = () => {
@@ -205,9 +213,7 @@ let handleDelList = () => {
 const handleEdit = (row) => {
   dialogFormVisible.value = true
   dialogType.value = "edit"
-  console.log(row)
   tableForm.value = { ...row }
-  tableDataCopy = Object.assign([],tableData.value)
   }
 </script>
 
